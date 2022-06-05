3ef43d101f42e176d315aefed2bf7c510676e56b
BBotzer <btb5103@gmail.com>
2022-06-05 17:53:04 -0400

    Update main.py
    main() driver has been created.  It will test the displayBoard file
    
    The main should remain clean from excessive function creations.  Put them in a different file.
    
    Other possible functions we need are located in the bottom of main().

--- main.py
+++ main.py
@@ -1,1 +1,53 @@
+"""
+
+This is the main driver for the Tic Tac Topia AI project.
+
+Authors:
+    Penn State AI-801, Summer 2022, TEAM 2
+    Brandon Botzer
+    Ambika Chundru
+    Nick Bartkowiak
+    
+
+Purpose:
+    TEXT
+    
+How to use this code:
+    TEXT
+
+
+"""
+
+"import the functions from displayBoard file"
+from displayBoard import *
+
+
+
+
+
+"test the displayBoard code"
+testdisplayBoard()
+
+
+
+"Do we need a function to count the number of turns?"
+
+
+"""
+Functions we still need:
+    
+    Check for win condition (probably easier with the grid)
+    
+    Check for which grid positions are open (no moves on that location yet)
+    
+    AI Solvers:
+        MiniMax
+        A*
+        Breadth
+        Depth
+        
+    OTHERS???
+"""
+
+
 
